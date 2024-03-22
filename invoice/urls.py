4bded96090dbfe9873149ebd675f9236455e58d6
Steve Han <steve.han11@gmail.com>
2024-03-22 11:03:32 -0700

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   invoice/urls.py
    	modified:   mytest/settings.py
    	modified:   mytest/urls.py
    	new file:   static/css/sb-admin-2.css
    	new file:   static/css/style.css

--- invoice/urls.py
+++ invoice/urls.py
@@ -3,8 +3,6 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-
-
     path('product', ProductEditView.as_view(extra_context={'add': True}), name='product-add'),
     path('product/<int:pk>/', ProductEditView.as_view(extra_context={'add': False}), name='product-edit'),
     path('product/list/', ProductListView.as_view(extra_context={'add': False}), name='product-list'),
--- mytest/settings.py
+++ mytest/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'formset',
+    'invoice.apps.InvoiceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'mytest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates/invoice'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +77,13 @@ WSGI_APPLICATION = 'mytest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mytest',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5434',
+
     }
 }
 
